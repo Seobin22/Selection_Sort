d6265d9eccdec08e8f1935cfe866ccdc51a59e52
Seobin22 <ssbin425@gmail.com>
2025-09-14 17:26:38 +0900

    for_step==1000

--- select.c
+++ select.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define size 10
+#define Max 1000000
 
 void change(int *first, int *after){
     int temp;
@@ -10,54 +10,61 @@ void change(int *first, int *after){
     *after = temp;
 }
 
-void sort(int *arr){
+void sort(int *arr,int an){
+    if (an>0){
     int mi;
     //0부터 끝-1 까지 돌아가면 현재 인덱스 ,, 현재 인덱스 +1 부터 끝까지 비교 . 
-    for(int top=0;top<size-1;top++){
+    for(int top=0;top<an-1;top++){
         mi=top;
-        for (int t=top+1;t<size;t++){
+        for (int t=top+1;t<an;t++){
             if(arr[mi] > arr[t]) mi=t;
             }    
             if (top!=mi) change(&arr[top],&arr[mi]);
         }                
 }
+}
 int is_sorted(int A[],int n){
     for (int i=0; i<n-1;i++){
         if (A[i]>A[i+1]){
             
-            printf("0");
+        
             return 0;
         }
     }
-    printf("1_____________-\n");
+    
 
     return 1;
 }
-void timecheck(int A[]){
+void timecheck(int A[],int an){
     /*for(step;step<10001;)*/
     clock_t start;
     double time_used;
     start = clock();
-    sort(A);
-;   time_used = ((double) (clock() - start)) / CLOCKS_PER_SEC;
-    printf("time : %f\n",time_used);
+    sort(A,an); 
+    time_used = ((double) (clock() - start)) / CLOCKS_PER_SEC;
+    printf("%10f",time_used);
+    if (is_sorted(A,an)==1) printf("%10s\n","정렬됨");
+    else printf("%10s\n","정렬안됨");
 }
 int D(void){
     int an,n=1,step=0;
-    int *list;
+    int *arr;
     while (1) {
         an=step+(n-1)*step;
-        printf("%d\n",an);
-
-        list=(int *)malloc(sizeof(int)*an);
-        if (list==NULL) {
+        printf("%10d",an);//배열크기
+/////////////////////////////////(arr 생성)
+        arr=(int *)malloc(sizeof(int)*an);
+        if (arr==NULL) {
             printf("no memory\n");
             return -1;
         }
-
-        // for(int i=0;i<size;i++){
-        // arr[i]=rand()%100000+1;
-        // }
+        if(an!=0){
+        for(int i=0;i<an;i++){
+        arr[i]=rand()%100000+1;
+        }}
+////////////////////////////////////
+        timecheck(arr, an);
+        free(arr);
 
 
         if (n==10 || step==0){
@@ -73,16 +80,8 @@ int D(void){
 
 int main(void)
 {   
-    printf("test ddddddddd  try\n");
+    printf("%10s %10s %10s\n","n","시간","정렬상태");
     srand(time(NULL));
-    int arr[size]={0};
-    for(int i=0;i<size;i++){
-        arr[i]=rand()%100000+1;
-    }
-
-    sort(arr);
-    is_sorted(arr,size);
-    printf("+________________");
     D();
     return 0;
 }
