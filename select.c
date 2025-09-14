87aa76369c0c8c373bbf0db37b1db399a8c25c0b
Seobin22 <ssbin425@gmail.com>
2025-09-14 23:42:26 +0900

    fianl_try

--- select.c
+++ select.c
@@ -127,6 +127,27 @@ int binsearch(int list[], int searchnum,int left, int right){
     }
     return -1;
 }
+int r_binsearch(int list[], int searchnum,int left, int right){
+    int counter=0;
+    int middle;
+    while (left<=right){
+        counter++;
+        middle=(left+right)/2;
+        switch(COMPARE(list[middle],searchnum)){
+            case -1:
+                right=middle-1;
+                break;
+            case 1:
+                left=middle+1;
+                break;
+            case 0:
+                return counter;
+        }
+
+    }
+    return -1;
+}
+
 
 // void testt(int searchnum1[]){
 //     int *arr;
@@ -148,7 +169,7 @@ int main(void)
     int *arr;
     int searchnum1[]={1,10,100,1000,10000,12500,50000,53125,75000,75195,100000};
     int searchnum2[]={120,121,122,123,124,125,126,127,128,129,130};
-
+    int searchnum3[]={5,50,195,500,781,5000,12548,25012,281245,37500,50000};
     printf("1.1. 무작위 100,000개 정수 생성 및 정렬, 정렬 검사 수행\n");
 
 
@@ -161,7 +182,7 @@ int main(void)
     printf("1.1.2. 배열 A에 무작위로 저장된 정수를 정렬\n");
     if (arr!=NULL)sort(arr,100000);
     for(int i=0;i<100;i++){
-        printf("%5d ",arr[i]%1000);
+        printf("%5d",arr[i]%1000);
     }
     printf("... 완료\n");
     printf("1.1.3.배열 A에 정렬된 정수의 정렬 상태 확인\n");
@@ -171,7 +192,7 @@ int main(void)
     printf("1.2. 배열의 크기를 100부터 1,000,000까지 바꾸면서 sort 함수의 실행 시간 측정\n");
 
     printf("%10s %10s %10s\n","n","시간","정렬상태");
-    D(100);
+    D(1000000);
     printf("1.3. 100,000개의 정수를 무작위로 생성하여 정렬 후 이진검색 수행\n1.3.1. 배열에 100,000개의 정수를 무작위로 생성하여 저장\n");
     arr=makearr(100000);
 
@@ -185,20 +206,41 @@ int main(void)
     for(int i=0;i<11;i++){
         int counter=binsearch(arr,searchnum1[i],0,99999);
         printf("검색 데이터( 1 ~ 100000 ) : %d\n",searchnum1[i]);
-        printf("검색 데이터 :        %d, 	Counter : %d",searchnum1[i],counter);
+        printf("검색 데이터 :        %d, 	Counter : %d\n",searchnum1[i],counter);
+    }
+    for (int i = 0; i < 11; i++) {
+        int counter = binsearch(arr, searchnum2[i], 0, 99999);
+        printf("검색 데이터( 1 ~ 100000 ) : %d\n", searchnum2[i]);
+        printf("검색 데이터 :        %d, 	Counter : %d\n", searchnum2[i], counter);
+    }
+    
+    printf("검색종료.\n1.4. 배열의 크기를 변화시키면서 counter의 값을 관찰\n1.4.1. 배열에 100000개의 정수를 역순으로 생성하여 저장\n");
+    printf("1.4.2. 배열에 저장된 100000개의 정수 정렬\n");
+    r_sort(arr,100000);
+
+    printf("완료.\n");
+    printf("1.4.3. 배열에 정렬된 정수의 정렬 상태 확인\n");
+    if(r_is_sorted(arr,100000)==1) printf("정렬완료!!!\n");
+    for(int i=0;i<11;i++){
+        int counter=r_binsearch(arr,searchnum1[i],0,99999);
+        printf("검색 데이터( 1 ~ 100000 ) : %d\n",searchnum1[i]);
+        printf("검색 데이터 :        %d, 	Counter : %d\n",searchnum1[i],counter);
     }
     free(arr);
+
     printf("검색종료\n1.4.1. 배열에 50000개의 정수를 역순으로 생성하여 저장\n");
+    
     arr=makearr(50000);
-    r_sort(arr,50000);
+
     printf("1.4.2. 배열에 저장된 50000개의 정수 정렬\n");
-    sort(arr,50000);
-    printf("완료.");
-    if(is_sorted(arr,50000)==1) printf("정렬완료!!!\n");
+    r_sort(arr,50000);
+    printf("완료.\n1.4.3. 배열에 정렬된 정수의 정렬 상태 확인\n");
+    if(r_is_sorted(arr,50000)==1) printf("정렬완료!!!\n");
+    
     for(int i=0;i<11;i++){
-        int counter=binsearch(arr,searchnum2[i],0,49999);
-        printf("검색 데이터( 1 ~ 50000 ) : %d\n",searchnum1[i]);
-        printf("검색 데이터 :        %d, 	Counter : %d",searchnum1[i],counter);
+        int counter=r_binsearch(arr,searchnum3[i],0,49999);
+        printf("검색 데이터( 1 ~ 50000 ) : %d\n",searchnum3[i]);
+        printf("검색 데이터 :        %d, 	Counter : %d\n",searchnum3[i],counter);
     }
     printf("검색종료\n");
     free(arr);
